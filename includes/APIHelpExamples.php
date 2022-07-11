6941e6107e97f19b32376f60d81457ededc96f42
BrandonXLF <brandfowl@gmail.com>
2022-07-10 23:49:05 -0400

    Don't specify protocol for API examples

--- includes/APIHelpExamples.php
+++ includes/APIHelpExamples.php
@@ -7,7 +7,7 @@ class APIHelpExamples implements ProducesHtml {
 
 	public function __construct(...$examples) {
 		$this->examples = $examples;
-		$this->prefix = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+		$this->prefix = "//{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
 	}
 
 	public function getHtml() {
